8258f0a4ac07ed00a5637c635a9d7c9fc1d18f99
severus <truongquynh2525@gmail.com>
2024-03-22 21:35:14 +0700

    dummy todo app

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,38 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue'
+
+type TodoItem = {
+  Name: string | undefined
+}
+
+const input = ref<TodoItem>({ Name: '' }) // Initialize input with an empty TodoItem
+const todoList = ref<TodoItem[]>([]) // Specify the type for todoList
+
+const handleOnClick = () => {
+  todoList.value.push({ Name: input.value.Name }) // Access the Name property directly without optional chaining
+  input.value.Name = ''
+}
+
+const handleOnRemove = (name?: string) => {
+  // remove the name from todo list
+  todoList.value = todoList.value.filter((todo) => todo.Name !== name)
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <!-- create an input and a button -->
+    <input type="text" v-model="input.Name" />
+    <!-- Access the Name property directly without optional chaining -->
+    <button @click="handleOnClick">Add new todo</button>
+    <!-- Remove parentheses from handleOnClick -->
+    <!-- render todoList -->
+    <div v-for="todo in todoList" :key="todo.Name">
+      <!-- Specify a unique key for the v-for loop -->
+      <p>{{ todo.Name }}</p>
+      <button @click="handleOnRemove(todo.Name)">Remove</button>
+    </div>
   </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
